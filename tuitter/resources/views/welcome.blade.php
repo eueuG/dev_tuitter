56fa9aab6aafc976dbafe7fc124c7abc53d8316b
aiai-github <95994841+aiai-github@users.noreply.github.com>
2022-02-13 13:06:35 +0900

    Update welcome.blade.php

--- tuitter/resources/views/welcome.blade.php
+++ tuitter/resources/views/welcome.blade.php
@@ -1,29 +1,55 @@
 <!doctype html>
-<html lang="ja">
+<html lang="ja" style="position: relative; min-height: 100%;">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
     <title>ついつたー</title>
   </head>
-  <body>
-    <div class="container p-5">
-        <div class="p-5">
-            <h1>ついつたー</h1>
-            <p>こんにちは，せかい。ついつたーはじめました。</p>
-            <button type="button" class="btn btn-primary">登録</button>
+  <body style="margin-bottom: 60px;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <h1><a class="navbar-brand" href="#">ついつたー</a></h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul class="navbar-nav mt-2">
+            <li class="nav-item me-2 mb-2">
+              <a class="nav-link" href="#">ついつたーとは</a>
+            </li>
+            <li class="nav-item me-2 mb-2">
+              <a class="nav-link" href="#">お問い合わせ</a>
+            </li>
+            <li class="nav-item me-2 mb-2">
+              <button type="button" class="btn btn-secondary d-block" style="width:100px">ログイン</button>
+            </li>
+          </ul>
         </div>
-        <div class="p-5">
-            <p>登録済みの方はこちら</p>
-            <button type="button" class="btn btn-light">ログイン</button>
+      </div>
+    </nav>
+    <div class="container p-0 mb-5">
+        <div class="p-0 p-sm-5">
+            <div class="d-block p-0 mx-auto mx-sm-0" style="max-width: 80%"><p class="display-1">こんにちは，<br>せかい。</p></div>
+            <div class="py-5">
+              <p class="d-block p-0 mx-auto mx-sm-0" style="width:300px">ついつたーをはじめる</p>
+              <button type="button" class="btn btn-primary d-block my-2 mx-auto mx-sm-0" style="width:300px">登録</button>
+            </div>
+            <div class="d-grid">
+              <p class="d-block p-0 mx-auto mx-sm-0" style="width:300px">登録済みの方はこちら</p>
+              <button type="button" class="btn btn-secondary d-block my-2 mx-auto mx-sm-0" style="width:300px">ログイン</button>
+            </div>
         </div>
-
-        <!-- Optional JavaScript; choose one of the two! -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    </div>
+    <footer class="footer" style="position: absolute; bottom: 0; width: 100%; height: 60px; background-color: #F8F9FA">
+      <div class="container">
+        <p style="margin: 20px 0">© 2022 ついつたー</p>
+      </div>
+    </footer>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
@@ -32,6 +58,5 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
         -->
-    </div>
   </body>
 </html>

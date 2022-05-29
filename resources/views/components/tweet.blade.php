7131236c6d1f220109acfde885474ee8776d56f3
eueuG <rinellie1228@icloud.com>
2022-05-30 00:35:23 +0900

    [add]画面コンポーネント追加
    部品はcomponentディレクトリに作る

---
+++ resources/views/components/tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="tubuyaki-wrapper">
+    <div class="tubuyaki">
+        <img src="{{ asset('img/{{  }}') }}" alt="user">
+        <div class="tubuyaki-user">{{ $tweet->user->name }}</div>
+        <div class="tubuyaki-tubuyaki">{{ $tweet->content }}</div>
+    </div>
+    <div class="tubuyaki-ripusuko">
+        <button class="tubuyaki-ripu"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M8.31 189.9l176-151.1c15.41-13.3 39.69-2.509 39.69 18.16v80.05C384.6 137.9 512 170.1 512 322.3c0 61.44-39.59 122.3-83.34 154.1c-13.66 9.938-33.09-2.531-28.06-18.62c45.34-145-21.5-183.5-176.6-185.8v87.92c0 20.7-24.31 31.45-39.69 18.16l-176-151.1C-2.753 216.6-2.784 199.4 8.31 189.9z"/></svg></button>
+        <button class="tubuyaki-suko"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg></button>
+    </div>
+</div>
+
